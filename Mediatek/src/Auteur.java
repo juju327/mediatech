ea3f2dfb8d21d609ed335cd1fb29d16e29ae2e88
shizukan <shizukansama@yahoo.fr>
2015-03-17 10:58:13 +0100

    Amélioration d'auteur

--- Mediatek/src/Auteur.java
+++ Mediatek/src/Auteur.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
+
 
 public class Auteur {
 	private String nom ;
 	private String prenom ;
 	private String nationalité ;
-	
+	private ArrayList<Document> aPublie ;
 	
 	public Auteur(String nom, String prenom, String nationalité) {
 		setNom(nom);
 		setPrenom(prenom);
 		setNationalité(nationalité);
+		aPublie = new ArrayList<Document>();
 	}
 
 	public String getNom() {
@@ -34,6 +37,31 @@ public class Auteur {
 	private void setNationalité(String nationalité) {
 		this.nationalité = nationalité;
 	}
+
+	//Pour l'ArrayList
+	public ArrayList<Document> getaPublie() {
+		return aPublie;
+	}
+
+	/**
+	 * Ajoute un document publié à la liste de l'auteur. 
+	 * A utiliser, notamment, lors de l'ajout d'un auteur dans la liste des auteurs du document
+	 * @param document : Document (que l'auteur a publié)
+	 */
+	public void addDocumentPublie(Document document){
+		getaPublie().add(document);
+	}
+
+	/**
+	 * Supprime un document publié à la liste de l'auteur.
+	 *A utiliser, notamment, lors de la suppression d'un auteur dans la liste des auteurs du document
+	 * @param document : Document (que l'auteur a publié)
+	 */
+	public void removeDocumentPublie(Document document){
+		getaPublie().remove(document);
+	}
+	
+	
 	
 	
 }
