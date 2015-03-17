f75f7afccfcc6d192eb3a992e893bbe6bd1e4afb
shizukan <shizukansama@yahoo.fr>
2015-03-17 10:12:47 +0100

    Class document

---
+++ Mediatek/src/Document.java
@@ -0,0 +1,51 @@
+
+public abstract class Document {
+	private String titre ;
+	private String dateParution ;
+	private String reference ;
+	private int dureeMax ;
+	private int nbMax ;
+	
+	
+	public String getTitre() {
+		return titre;
+	}
+	
+	private void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	public String getDateParution() {
+		return dateParution;
+	}
+	
+	private void setDateParution(String dateParution) {
+		this.dateParution = dateParution;
+	}
+	
+	public String getReference() {
+		return reference;
+	}
+	
+	private void setReference(String reference) {
+		this.reference = reference;
+	}
+	
+	public int getDureeMax() {
+		return dureeMax;
+	}
+	
+	private void setDureeMax(int dureeMax) {
+		this.dureeMax = dureeMax;
+	}
+	
+	public int getNbMax() {
+		return nbMax;
+	}
+	
+	private void setNbMax(int nbMax) {
+		this.nbMax = nbMax;
+	}
+	
+	
+}
