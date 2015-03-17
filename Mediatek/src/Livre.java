c5dc44747f762102431e12d8838c774b307a551b
shizukan <shizukansama@yahoo.fr>
2015-03-17 10:26:45 +0100

    Ajout de musique

--- Mediatek/src/Livre.java
+++ Mediatek/src/Livre.java
@@ -1,4 +1,13 @@
 
-public class Livre {
-
+public class Livre extends Document {
+	private String numeroISBN ;
+	//private GenreLivre genre ;
+	
+	
+	public Livre(String titre, String dateParution, String reference,
+			int dureeMax, int nbMax) {
+		super(titre, dateParution, reference, dureeMax, nbMax);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
---
+++ Mediatek/src/Musique.java
@@ -0,0 +1,10 @@
+
+public class Musique extends Document{
+
+	public Musique(String titre, String dateParution, String reference,
+			int dureeMax, int nbMax) {
+		super(titre, dateParution, reference, dureeMax, nbMax);
+		// TODO Auto-generated constructor stub
+	}
+
+}
